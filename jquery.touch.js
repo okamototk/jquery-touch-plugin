5048ee2a463096d8fae7d989ea8de2542e5fd46a
Takashi Okamoto <toraneko at gmail.com>
2012-01-21 07:26:20 +0900

    Brashup plugin.

--- jquery.touch.js
+++ jquery.touch.js
@@ -1,90 +1,111 @@
-/*! Copyright (c) 2009 Brandon Aaron (http://brandonaaron.net)
- *  Copyright (c) 2011 Takashi Okamoto
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
- * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
- *
- * This plugin is developed under GitHub:
- *
- *  https://github.com/okamototk/jquery-touch-plugin
+/*!
+ * jQuery Touch Event Plugin 0.8
  * 
- * Version: 0.9.0
- * 
- * Requires: 1.2.2+
+ * https://github.com/okamototk/jquery-touch-plugin
+ *
+ * Copyright 2012 Takashi Okamoto
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
  */
-
 (function($) {
-/** for gesturechange **/
-$.event.special.gesturechange = {
-	setup: function() {
-		if ( this.addEventListener )
-			this.addEventListener( 'gesturechange', handler, false );
-		else
-			this.ongesturechange = handler;
-	},
-	
-	teardown: function() {
-		if ( this.removeEventListener )
-			this.removeEventListener( 'gesturechange', handler, false );
-		else
-			this.ongesturechange = null;
-	}
-};
 
-$.fn.extend({
-	gesturechange: function(fn) {
-		return fn ? this.bind("gesturechange", fn) : this.trigger("gesturechange");
-	},
-	
-	ungesturechange: function(fn) {
-		return this.unbind("gesturechange", fn);
-	}
-});
+  $.event.special.touchmove = {
 
+    setup: function() {
+      if ( this.addEventListener ){
+        this.addEventListener( "touchmove", handler, false );
+        this.addEventListener( "touchend", touchendHandler, false );
+      } else {
+        this.ontouchmove = handler;
+      }
+    },
 
-function handler(event) {
-	var args = [].slice.call( arguments, 1 );
-	event = $.event.fix(event || window.event);
-	event.type = "gesturechange";
-	args.unshift(event, {scale:event.scale, rotation:event.rotation});
-	return $.event.handle.apply(this, args);
-}
+    teardown: function() {
+      if ( this.removeEventListener ) {
+        this.removeEventListener( "touchmove", handler, false );
+        this.removeEventListener( "touchend", touchendHandler, false );
+      } else {
+        this.ontouchmove = null;
+      }
+    }
+    
+  };
 
-/** for touchmove **/
-$.event.special.touchmove = {
-	setup: function() {
-		if ( this.addEventListener )
-			this.addEventListener( 'touchmove', handler, false );
-		else
-			this.ontouchmove = handler;
-	},
-	
-	teardown: function() {
-		if ( this.removeEventListener )
-			this.removeEventListener( 'touchmove', handler, false );
-		else
-			this.ongesturechange = null;
-	}
-};
+  function touchendHandler(event){
+    _gestureInit = false;
+  }
 
-$.fn.extend({
-	gesturechange: function(fn) {
-		return fn ? this.bind("touchmove", fn) : this.trigger("touchmove");
-	},
-	
-	ungesturechange: function(fn) {
-		return this.unbind("touchmove", fn);
-	}
-});
+  var _scrLen = 0,_srcX,_srcY;
+  var _gestureInit = false;
 
+  function initGesture(e){
+    _gestureInit = true;
+    var x1 = e.touches[0].pageX;
+    var y1 = e.touches[0].pageY;
+    var x2 = e.touches[1].pageX;
+    var y2 = e.touches[1].pageY;
+    _srcX = x2-x1;
+    _srcY = y2-y1;
+    _srcLen = Math.sqrt(_srcX*_srcX+_srcY*_srcY);
+  }
 
-function handler(event) {
-	var args = [].slice.call( arguments, 1 );
-	event = $.event.fix(event || window.event);
-	event.type = "touchmove";
-	args.unshift(event, {scale:event.scale,rotation:event.rotation,touches:event.touches,targetTouches:event.targetTouches,changedTouches:event.changedTouched});
-	return $.event.handle.apply(this, args);
-}
+  function calcGesture(e){
+    
+    if(e.touches.length==2){
+      if(!_gestureInit){
+        initGesture(e);
+      } else {
+        var
+          x1 = event.touches[0].pageX,
+          y1 = event.touches[0].pageY,
+          x2 = event.touches[1].pageX,
+          y2 = event.touches[1].pageY,
+          destX = x2-x1,
+          destY = y2-y1,
+
+        // calc scale
+        destLen = Math.sqrt(destX*destX+destY*destY);
+        e.scale = destLen / _srcLen;
+
+        // calc rotation
+        var
+          ip = _srcX * destX + _srcY * destY, // innter product
+          ep = _srcX * destY - _srcY * destX, // exterior product
+          cos = ip / ( _srcLen * destLen ),
+          sin = ep / ( _srcLen * destLen );
+        if ( ip != 0 ){
+          e.rotation = Math.atan( ep / ip ) * 360 / ( Math.PI * 2 );
+        } else {
+          e.rotation = 90;
+        }
+        if ( cos < 0 ) {
+          if ( sin > 0 ) {
+            e.rotation = e.rotation + 180;
+          } else {
+            e.rotation = e.rotation - 180;
+          }
+        }
+        
+      }
+    }
+  }
+
+  function handler(event) {
+    origEvent = event;
+    event = $.event.fix( event || window.event );
+    event.type = "touchmove";
+    event.touches = origEvent.touches;
+    
+    if( typeof(origEvent.scale) === "undefined" ){
+      calcGesture(event);
+    } else {
+      event.scale = origEvent.scale;
+      event.rotation = origEvent.rotation;
+    }
+
+    var args = [this,event];
+    args.unshift(event);
+    return $.event.handle.apply(this, args);
+  }
 
 })(jQuery);
